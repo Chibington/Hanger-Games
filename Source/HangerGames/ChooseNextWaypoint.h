ce9a8a11cdc73cc2f11fb5b1deb960c8e3219c85
Chibitybibityba <chrisjokim@gmail.com>
2017-11-15 21:16:52 -0500

    Began Switching Behavior Trees to C++

---
+++ Source/HangerGames/ChooseNextWaypoint.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANGERGAMES_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+		virtual EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
+	
+};
