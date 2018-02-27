b84a39a51885cc2acac9ff101941e628211356fe
Chibitybibityba <chrisjokim@gmail.com>
2018-02-26 22:24:19 -0500

    Finished Basic Movement in C++, Imported Anim Starter, Cleaned AI BPs

--- Source/HangerGames/PatrolRoute.h
+++ Source/HangerGames/PatrolRoute.h
@@ -18,7 +18,7 @@ public:
 	TArray<AActor*> GetPatrolPoint() const;
 
 private:
-	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+	UPROPERTY(EditAnywhere, Category = "Patrol Route")
 		TArray<AActor*> PatrolPoints;
 
 
