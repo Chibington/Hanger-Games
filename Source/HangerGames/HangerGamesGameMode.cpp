13840c7ff4ecf2c981260e309a2be53193bf184b
Chibitybibityba <chrisjokim@gmail.com>
2018-03-21 23:27:27 -0400

    Moved Some Assets into Non-LFS Folder

--- Source/HangerGames/HangerGamesGameMode.cpp
+++ Source/HangerGames/HangerGamesGameMode.cpp
@@ -9,7 +9,7 @@ AHangerGamesGameMode::AHangerGamesGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
